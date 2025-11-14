3e07cc15ea08122b9af9442119c6c0e5005617cb
Manoj Kumar <manojthakurkkr@gmail.com>
2025-11-14 12:02:17 +0530

    store todo items

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 import json
+from dotenv import load_dotenv
+import pymongo
+from pymongo.errors import PyMongoError
+import os 
+
+
+load_dotenv() 
+
+MONGO_URL = os.getenv("MONGO_URL")
+client = pymongo.MongoClient(MONGO_URL)
+db = client.cluster0
+collection = db['todo']
 
 app = Flask(__name__)
 
@@ -8,6 +20,22 @@ DATA_FILE = "data.txt"
 @app.route('/')
 def home():
     return "Welcome to new kingdom"
+
+
+@app.route('/submittodoitem',methods=['POST'])   
+def submit_todo_item():
+    name = request.json.get('itemName')
+    description = request.json.get('itemDescription')
+    todo_item = {
+        "itemName": name,
+        "itemDescription": description
+    }
+    inserted = collection.insert_one(todo_item)
+    if inserted.acknowledged: 
+        return "to do saved successfully!" 
+    else:
+        message = "something went wrong!" 
+    return redirect(url_for('home', status=message)) 
 
 @app.route('/api')
 def api():
